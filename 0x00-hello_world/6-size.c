c3c7b4a466ea4b465fd5c4664b2f8ec70952f430
vagrant <vagrant@precise64.(none)>
2019-06-18 21:38:56 +0000

    [FEATURE] printing sizes of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+printf("size of a char: %d bytes(s)\n", sizeof(char));
+printf("size of an init: %d byte(s)\n", sizeof(int));
+printf("size of a long init: %d bytes(s)\n", sizeof(long));
+printf("size of a long long init: %d bytes(s)\n", sizeof(long long));
+printf("size of a float: %d bytes(s)\n", sizeof(float));
+return (0);
+}
