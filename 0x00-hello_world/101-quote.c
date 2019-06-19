a969187e0aa53745042d1c1b19fe917b4cf9aee6
vagrant <vagrant@precise64.(none)>
2019-06-19 00:44:28 +0000

    [FEATURE] writing a text without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char z[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(0, z, (61));
+return (-1);
+}
