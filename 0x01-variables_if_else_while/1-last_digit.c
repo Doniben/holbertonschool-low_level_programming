b9a23363ed6f71a30930a9144a7b8d28b291e515
vagrant <vagrant@precise64.(none)>
2019-06-19 10:36:44 +0000

    [FEATURE] identifying the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+int x;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+x = n % 10;
+if (n > 5)
+{
+printf("Last digit of %d is %dx and is greater than 5\n", n, x);
+}
+else
+{
+if (n == 0)
+{
+printf("Last digit of %d is %dx and is 0\n", n, x);
+}
+else
+{
+printf("Last digit of %d is %dx and is less than 6 and not 0\n", n, x);
+}
+}
+return (0);
+}
