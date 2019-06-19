ed8a89a685b51249f6afb42547705db9e5b83f0a
vagrant <vagrant@precise64.(none)>
2019-06-19 15:24:29 +0000

    [FEATURE] printing numbers with commas and spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,14 @@
 int main(void)
 {
 int ch;
-for (ch = 48; ch < 58; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
 putchar(ch);
+if( ch != 57 )
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
