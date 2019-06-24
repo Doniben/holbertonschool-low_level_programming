c90281ab99f5da51b9a82e1f3bb781c21d410499
vagrant <vagrant@precise64.(none)>
2019-06-24 17:49:59 +0000

    [FEATURE] Checking signs with own function print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - check the code for Holberton School students.
+ * @n: prints the character.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
