5002bcb1846fccb97680ad343da763b3f436b54f
vagrant <vagrant@precise64.(none)>
2019-06-24 13:58:12 +0000

    [FEATURE] Checking lower and upercases

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if(c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
