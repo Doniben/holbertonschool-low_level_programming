b5964737bd1fc9c6aa03157705951dc295a69cf0
vagrant <vagrant@precise64.(none)>
2019-06-24 17:18:26 +0000

    [FEATURE] checking lower and uppercases with own function isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _isalpha - check the code for Holberton School students.
+ * @c: prints the character.
+ * 
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-	if(c >= 65 && c <= 122)
+	if (c >= 65 && c <= 122)
 	{
 		return (1);
 	}
