21e106c6512de583147769252787747a46479257
vagrant <vagrant@precise64.(none)>
2019-06-25 15:13:53 +0000

    [FEATURE] triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Function to print a triangle
+ * @size: variable triangle
+ *
+ */
+
+void print_triangle(int size)
+{
+	int d, o;
+
+	if (size > 0)
+	{
+		for (d = 0; d < size; d++)
+		{
+			for (o = 0; o < size; o++)
+				if (o < size - (d + 1))
+				{
+					_putchar(' ');
+				}
+				else
+					_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
