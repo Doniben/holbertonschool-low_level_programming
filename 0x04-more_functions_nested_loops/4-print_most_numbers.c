95462e330c8218e17e752721c2118fd7cade3314
vagrant <vagrant@precise64.(none)>
2019-06-25 08:32:25 +0000

    [FEATURE] printing more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Printing numbers.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+		_putchar(a);
+		}
+	}
+	_putchar(10);
+}
