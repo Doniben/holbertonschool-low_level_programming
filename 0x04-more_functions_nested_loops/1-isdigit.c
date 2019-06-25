374d4df9df67535ffb99d4c0e1dc4de4d1da010a
vagrant <vagrant@precise64.(none)>
2019-06-25 06:55:01 +0000

    [FEATURE] Checking for digits returning to 1 & 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Uppercase checker.
+ * @c: variable to be checked.
+ * Return: to 1 if uppercase.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
