2cc784cf42bea4efe61b7f126055e2bd9c7415d3
vagrant <vagrant@precise64.(none)>
2019-06-25 07:55:45 +0000

    [FEATURE] printing numbers with new function from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Printing numbers.
+ *
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar(10);
+}
