44d314d6d6b07c6d4aec5cf519e0d438e0270fd0
vagrant <vagrant@precise64.(none)>
2019-06-25 00:12:09 +0000

    [FEATURE] creating a new sum function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**                                                                                             
+ * print_last_digit - check the code for Holberton School students.                             
+ *                                                                                       
+ * Return: Always 0.                                                                            
+ */
+
+int add(int x, int y)
+{
+	int sum;
+	sum = x + y;
+	return (sum);
+}
