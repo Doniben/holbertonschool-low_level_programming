2df96a835c180f9e94cae9615c1294eeacba3703
vagrant <vagrant@precise64.(none)>
2019-06-25 08:09:47 +0000

    [FEATURE] Checking if uppercase retutning to 1 & 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * isuper - Uppercase checker.
- * @c: variable to be checked.
+li * @c: variable to be checked.
  * Return: to 1 if uppercase.
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 &&c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 		return (0);
