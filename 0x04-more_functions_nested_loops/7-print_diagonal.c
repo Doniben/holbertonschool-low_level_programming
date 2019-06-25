41042c678298c56058c765b9ca2bfa566da8c1ce
vagrant <vagrant@precise64.(none)>
2019-06-25 10:42:37 +0000

    [FEATURE] Printing a diagonal with two loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Function to print a diagonal.
+ * @n: variable to print the diagonal.
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+		_putchar(10);
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+		{
+			if (a == b)
+			{
+				_putchar(92);
+				break;
+			}
+			else
+			_putchar(32);
+		}
+		_putchar(10);	
+	}
+}
