01e98066d5bb78679e160bad2a2513f6ca165e99
vagrant <vagrant@precise64.(none)>
2019-06-25 12:25:25 +0000

    [FEATURE] Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * 
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+	int s;
+
+	for (s = 1; s <= 100; s++)
+	{
+		if (s % 3 == 0 && s % 5 != 0)
+			printf("Fizz");
+		if (s % 3 != 0 && s % 5 == 0)
+			printf("Buzz");
+		if (s % 3 == 0 && s % 5 == 0)
+			printf("FizzBuzz");
+		if (s % 3 != 0 && s % 5 != 0)
+			printf("%d", s);
+		printf(" ");
+	}
+	return (0);
+}
