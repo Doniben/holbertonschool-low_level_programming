36698dc6a1fca4806bacac39774a961b3a3983ae
vagrant <vagrant@precise64.(none)>
2019-07-02 07:49:35 +0000

    [FEATURE] printing a string using a while, an own function and a pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - Write a string with a new line
+ * @str: The string that receives
+ * Return: On success 1.
+ * 
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (*(str + count))
+	{
+		_putchar(*(str + count));
+		count++;
+	}
+	_putchar(10);
+}
