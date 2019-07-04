83bbe857c57355ef6751dbae41e88ece08f15a21
vagrant <vagrant@precise64.(none)>
2019-07-04 14:36:46 +0000

    [FEATURE] concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - Concat two strings
+ * @dest: The string that receives
+ * @src: The second string that receives
+ *
+ * Return: On success.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int q = 0;
+	int x = 0;
+
+	while (*(dest + q))
+	{
+		q++;
+	}
+	while (*(src + x))
+{
+	*(dest + q) = *(src + x);
+	x++;
+	q++;
+}
+	return (dest);
+}
