5689941df05fb74cd0b44a7919b63f435a6aceaa
vagrant <vagrant@precise64.(none)>
2019-07-05 03:19:30 +0000

    [FEATURE] infinitite

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_buffer - buffer.
+ * @*b: string1.
+ * @size: string2
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char *b, int size);
+{
+	char *b = b - 1;
+	int size = &b;
+	if (size == b)
+	{
+       
+	}
+}
