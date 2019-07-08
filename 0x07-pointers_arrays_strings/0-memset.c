d513a68711e1da27d0382a23248edd25a3cd5db9
vagrant <vagrant@precise64.(none)>
2019-07-08 17:35:28 +0000

    [F]

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - entry point
+ * @s: array done 
+ * @b: value
+ * @n: unsigned int
+ * Return: to the array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int d;
+
+	for (d = 0 ; d < n ; d++)
+	{
+		s[d] = b;
+
+	}
+	return (s);
+}
