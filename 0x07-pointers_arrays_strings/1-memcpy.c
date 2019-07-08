e033263b7573e84c2409a62a2408981b086366e8
vagrant <vagrant@precise64.(none)>
2019-07-08 18:46:40 +0000

    [FEATURE] copying secific area in the memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - entry point
+ * @dest: array
+ * @src: value
+ * @n: int
+ * Return; to the array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int d;
+
+	for (d = 0 ; d < n ; d++)
+	{
+		*(dest +d) = *(src + d);
+
+	}
+	return (dest);
+}
