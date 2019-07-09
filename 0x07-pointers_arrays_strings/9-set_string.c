0ba7cce1cd57b8d24021d42029bdaaa1f3127e17
vagrant <vagrant@precise64.(none)>
2019-07-09 01:01:03 +0000

    [FEATURE] pointer to pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - Function that return the number that coincide two strings
+ * @s: The character to print
+ * @to: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
