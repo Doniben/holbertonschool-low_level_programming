c722c397a5c1066ae86244d4e1e3a87da671246d
vagrant <vagrant@precise64.(none)>
2019-07-09 15:00:10 +0000

    [FEATURE] return to the lenght of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _print_rev_recursion - return the lenght of a string.
+ * _strlen_recursion - return the lenght of a string.
  * @s: String who receives
  *
  * Return: Always to 0.
@@ -10,10 +10,10 @@ int _strlen_recursion(char *s)
 {
 	if (*(s) != 0)
 	{
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
