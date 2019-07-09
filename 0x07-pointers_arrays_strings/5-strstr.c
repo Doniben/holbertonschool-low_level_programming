c4690058cfb1c0144c9737cc9fd3700f107b13be
vagrant <vagrant@precise64.(none)>
2019-07-09 01:43:41 +0000

    [FEATURE] Function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,13 +14,14 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *a = haystack;
 		char *b = needle;
+
 		while (*haystack && *b && *haystack == *b)
 		{
 			haystack++;
 			b++;
 		}
 		if (!*b)
-			return a;
+			return (a);
 		haystack = a + 1;
 	}
 	return (0);
