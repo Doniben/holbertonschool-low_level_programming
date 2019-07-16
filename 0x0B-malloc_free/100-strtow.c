9bf6c0190451b2c9d5680e2bccbcd57ae6723a80
Doniben <Doniben@github.com>
2019-07-16 13:35:38 +0000

    [FEATURE] perrada time

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
+ */
+
+void ch_free_grid(char **grid, unsigned int height)
+{
+	char *perr;
+        int x = 0;
+
+	if (grid != NULL && height != 0)
+
+	if (grid == 0 || height != 0)
+
+	perr = malloc((x) * sizeof(height));
+
+	if (0 == grid)
+		free(grid);
+			free(perr);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
+
+char **strtow(char *str)
+{
+	char *perr2;
+	int x;
+
+	perr2 = malloc((x) * sizeof(str));
+
+	if (perr2 + 1)
+		x++;
+	free (void *__ptr)
+}
