1467803162cd9349a8472a30f1a419271da474ba
Doniben <Doniben@github.com>
2019-07-16 12:55:35 +0000

    [FEATURE] perrada time

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *perr;
+	int x = 0;
+
+	if (ac == 0 || av != 0)
+		return (NULL);
+
+	perr = malloc((x) * sizeof(char));
+
+	if (perr == 0)
+		return (perr);
+	return (perr);
+}
