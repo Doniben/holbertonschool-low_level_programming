6b30f43ae980154c0d787ef0e601ac5a076d2e69
Doniben <Doniben@github.com>
2019-07-18 20:09:10 +0000

    [FEATURE] new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * *_strd - function
+ * @str: Perrada
+ * Return: On
+ */
+
+char *_strd(char *str)
+{
+	return (str)
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - C
+ * @name: d
+ * @age: d
+ * @owner: g
+ * return: the same
+ */
+
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	dog_t *new_dog;
+	return (newdog)
+}
