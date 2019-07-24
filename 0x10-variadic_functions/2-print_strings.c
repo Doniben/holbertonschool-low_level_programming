228e82cf27a43e392e9ee0259d4d279cec08497e
Doniben <doniben@esperanto.co>
2019-07-24 17:43:02 +0000

    [FEATURE] function that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg_ptr;
+	unsigned int counter;
+	char *strf;
+
+	va_start(arg_ptr, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		strf = va_arg(arg_ptr, char *);
+		if (strf != NULL)
+			printf("%s", strf);
+		else
+			printf("%p", strf);
+		if (separator != NULL && (counter + 1) != n)
+			printf("%s", separator);
+	}		
+	printf("\n");
+	va_end(arg_ptr);
+}
