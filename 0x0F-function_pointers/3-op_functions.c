6283177fb944c2e7833ed0aa16ce485aecf46fea
Doniben <doniben@esperanto.co>
2019-07-24 03:28:50 +0000

    [FEATURE] functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,35 @@
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
- * op_add - 
- * @a:
- * @b:
- * Return:
+ * op_add - function to addition.
+ * @a: adding up
+ * @b: second adding up
+ * Return: return the result.
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - 
- * @a:
- * @b:
- * return:
+ * op_sub - substration
+ * @a: first number
+ * @b: minuend
+ * Return: return the substraction
  */
 
 int op_sub(int a, int b)
 {
-	return (a - b)
+	return (a - b);
 }
 
 /**
- * op_mul - 
- * @a:
- * @b:
- * return:
+ * op_mul - multiply function
+ * @a: first multiple
+ * @b: second multiple
+ * Return: return the multiplication result
  */
 
 int op_mul(int a, int b)
@@ -36,25 +38,35 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div -
- * @a:
- * @b:
- * return:
+ * op_div - division function
+ * @a: division quotien
+ * @b: divident
+ * Return: return the division result
  */
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - 
- * @a:
- * @b:
- * Return:
+ * op_mod - module function
+ * @a: first number to modulate
+ * @b: modulator
+ * Return: return to the modulation
  */
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
