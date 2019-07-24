9835f954d04b0b99705a1d6c36644d00800b5e6f
Doniben <doniben@esperanto.co>
2019-07-24 01:51:08 +0000

    [FEATURE] operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - 
+ * @a:
+ * @b:
+ * Return:
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - 
+ * @a:
+ * @b:
+ * return:
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b)
+}
+
+/**
+ * op_mul - 
+ * @a:
+ * @b:
+ * return:
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -
+ * @a:
+ * @b:
+ * return:
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - 
+ * @a:
+ * @b:
+ * Return:
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
