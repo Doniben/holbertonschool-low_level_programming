76e6f0ac793772487b1aa75a9316b8e98d026f30
Doniben <doniben@esperanto.co>
2019-07-24 03:26:58 +0000

    [FEATURE] calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,9 +4,36 @@
 
 /**
  * main - Entry point to the calc
- * @args:
- * @argv:
- * return:
+ * @argc: argument
+ * @argv: second value
+ * Return: Return to 0.
  */
 
-int main (int args, char *argv)
+int main(int argc, char *argv[])
+{
+	int oper;
+	int num1;
+	int num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-'
+	    && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argv[2][0] == '/' || argv[2][0] == '%')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	oper = get_op_func(argv[2])(num1,num2);
+	printf("%d\n", oper);
+	return (0);
+}
