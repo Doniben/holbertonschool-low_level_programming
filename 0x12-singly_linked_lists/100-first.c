7fb3fd1e53f967d5de24cfd2db17cae778ec0a35
Doniben <doniben@esperanto.co>
2019-07-31 01:48:38 -0400

    [FEATURE] printing before the execution of the main with the macro __atribute__((constructor)) + betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 /**
  * first - Entry point
  */
-void first()__attribute__((constructor));
-void first()
+void first(void)__attribute__((constructor));
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
