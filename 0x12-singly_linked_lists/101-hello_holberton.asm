4bd668450cb82dc7cb4accec12ce3b594f5b1a3b
Doniben <doniben@esperanto.co>
2019-07-31 01:26:30 -0400

    [FEATURE]  Hello Holberton in assembler

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,17 @@
-section .programFlow
-	    global main
+section .data
+	msg db 'Hello, Holberton',0xa
+	msglen equ $ - msg
+
+	section .text
+	global main
+
 main:
-	        mov edx,len
-	        mov ecx,msg
-	        mov ebx,0x1    	;select STDOUT stream
-	        mov eax,0x4    	;select SYS_WRITE call
-	        int 0x80       	;invoke SYS_WRITE
-	        mov ebx,0x0    	;select EXIT_CODE_0
-	        mov eax,0x1    	;select SYS_EXIT call
-	        int 0x80       	;invoke SYS_EXIT
-	section .programData
-msg:			 db "Hello, Holberton",0xa
-len:			 equ $ - msg
+	mov rax,1
+	mov rdi,1
+	mov rsi,msg
+	mov rdx,msglen
+	syscall
+
+	mov rax,60
+	mov rdi,0
+	syscall
