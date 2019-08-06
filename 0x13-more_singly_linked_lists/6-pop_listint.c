68bfaa975320562522df0199816ffca75d0cd2fc
Doniben <doniben@esperanto.co>
2019-08-06 14:09:53 -0400

    [FEATURE]  deletes the head node of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list, 
+ * and returns the head node’s data (n).
+ * @head: head of the list
+ * Return:  the errased element.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *provizora = *head;
+	int n;
+
+	if(!head)
+		return (0);
+	provizora = (*head)->next;
+	n = ((*head)->n);
+	free(*head);
+	*head = provizora;
+	return (n);
+}
