98518a6f1f217b46838646db08ce7987d21d0e26
Doniben <doniben@esperanto.co>
2019-08-06 17:25:43 -0400

    [FIX #1] function that returns the sum of all the data (n) of a listint_t linked list. + coments

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -11,11 +11,13 @@ int sum_listint(listint_t *head)
 {
 	int aldoni;
 	listint_t *kopi = head;
-
+	
 	if (!kopi)
 		return (0);
 	while (kopi)
+	{
 		aldoni += kopi->n;
-	kopi = kopi->next;
+		kopi = kopi->next;
+	}	
 	return (aldoni);
 }
