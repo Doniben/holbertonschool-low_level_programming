99824ee965011db196765334a37a7ed5df24e949
Doniben <doniben@esperanto.co>
2019-08-08 18:35:49 -0400

    [FEATURE] prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_binary - prints the binary representation of a number.
+ * @n: int number to convert
+ * Return: void function
  */
 
 void print_binary(unsigned long int n)
 {
-	int bin;
-	if (n > 0)
+	if (n)
 	{
-		bin = n >> 1;
+		print_binary(n >> 1);
 		if (n & 1)
 			_putchar('1');
 		else
