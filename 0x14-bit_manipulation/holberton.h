6187676b2cbc16f5da15be5e93adb7a7678c5e7b
Doniben <doniben@esperanto.co>
2019-08-08 15:36:34 -0400

    [FEATURE] library + libraries

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,11 @@
 #ifndef HOLBER_H
 #define HOLBER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -8,5 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _putchar(char c);
 
 #endif
