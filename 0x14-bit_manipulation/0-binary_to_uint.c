e77e6796579f9038f5fcda5b74c891746f9acf9a
Doniben <doniben@esperanto.co>
2019-08-08 16:07:36 -0400

    [FEATURE] converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary string
+ * Return: to the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int aldoni;
+	if (b == NULL)
+		return (0);
+	
+	aldoni = 0;
+	while (*b)
+	{
+		if (*b == 1)
+			aldoni = b <<= 1;
+		else if(*b == 0)
+			aldoni = b <<= 1;
+		else
+			return (0);
+		b++;
+	}
+	return (aldoni);
+}
