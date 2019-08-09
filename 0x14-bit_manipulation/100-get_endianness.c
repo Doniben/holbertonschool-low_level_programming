3a971d7bf37c29a7520add4fb6d58005367e8846
Doniben <doniben@esperanto.co>
2019-08-09 17:41:48 -0400

    [FEATURE] checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - Function that checks the endianness
+ * Return: to 0 if it is a big endian or 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *p = (char *) &i;
+	
+	if (p[0] == 1) /*"Little Endian*/
+	{
+		return (1);
+	}
+	else /*Big Endian*/
+		return (0);
+}
