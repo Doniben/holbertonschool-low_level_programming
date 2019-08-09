0532c2114aef04b7102e1447f531ed52d438f69c
Doniben <doniben@esperanto.co>
2019-08-08 21:03:01 -0400

    [FEATURE] sets the value of a bit to 0 at a given index.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index. 
+ * @n: value 
+ * @index: position of the bit to change
+ * Return: -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int val;
+
+	if (index > 63)
+		return (-1);
+	val = 0;
+	val = 1 << index;
+	*n = *n & ~val;
+	return (1);
+}
