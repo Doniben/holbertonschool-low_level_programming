4764fa51a810188a93da38abf313dd9020ceb505
Doniben <doniben@esperanto.co>
2019-08-14 06:56:43 -0400

    [FEATURE] Function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,7 @@
 
 int create_file(const char *filename, char * text_content)
 {
-	int fd;
-	char *buf = malloc(sizeof(char) * text_content);
+	int fd, c = 0;
 
 	if (!filename)
 		return (-1);
@@ -18,9 +17,11 @@ int create_file(const char *filename, char * text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (buf == NULL)
+	if (text_content == NULL)
 		text_content = "";
-	write(STDOUT_FILENO, buf, text_content);
+	while (text_content[c])
+		c++;
+	write(fd, text_content, c);
 	close(fd);
 	return (1);
 }
