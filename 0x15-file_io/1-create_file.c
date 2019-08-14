9070b6dbd164da8f4396f60eeedfeca562ede42b
Doniben <doniben@esperanto.co>
2019-08-14 06:44:56 -0400

    [FEATURE] Function to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * create_file - Function to create a file
+ * @filename: file to create
+ * @text_content: file to
+ * Return: 1 on Success.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *buf = malloc(sizeof(char) * text_content);
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (buf == NULL)
+		text_content = "";
+	write(fd, text_content, buf);
+	close(fd);
+	return (1);
+}
