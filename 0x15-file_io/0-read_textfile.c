a6703df58f2511a6bcfbdef63c148180aecb19ec
Doniben <doniben@esperanto.co>
2019-08-13 22:43:56 -0400

    [FEATURE] reads a text file and prints it to the POSIX standard output + betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,21 +4,21 @@
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: file to read and print
  * @letters: number of letters it should read and print
- * Return: 
+ * Return: to the length of characters.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_to_op, _read, _write;
-	char *buf = malloc(sizeof(char) *letters);
+	char *buf = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (0); 
+		return (0);
 /* Open */
 	file_to_op = open(filename, O_RDONLY);
 	if (file_to_op == -1)
 		return (0);
-	
+
 	/*buf[letters] = '\0';*/
 	if (buf == NULL)
 		return (0);
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 /* Read */
 	_read = read(file_to_op, buf, letters);
 	if (_read == -1)
-		return(0);
+		return (0);
 
 /* Write */
 	_write = write(1, buf, _read);
