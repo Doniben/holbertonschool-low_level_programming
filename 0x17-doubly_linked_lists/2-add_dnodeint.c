ab8bf1fc63b88360f02e3113d13c9be2b16dd7db
Doniben <doniben@esperanto.co>
2019-09-13 19:46:45 -0400

    [FEATURE] function that adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
-	new->prev = n;
+	new->prev = NULL;
 	if (*head)
 		(*head)->prev = new;
 	*head = new;
