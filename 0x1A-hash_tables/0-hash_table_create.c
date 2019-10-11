412e6af89269da471fbb795cf6b299086fa58afd
Doniben <doniben@esperanto.co>
2019-10-11 23:34:47 +0000

    [FEATURE] function that creates a hash table + betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,22 @@
 #include <hash_tables.h>
 
 /**
-* has_table_create - creates a new hash table.
+* hash_table_create - creates a new hash table.
 * @size: size of the array
 * Return: NULL if something is wrong.
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t = *hash_n;
+	hash_table_t = *hash_n;
 
-  if (!size)
-    {
-      hash_n = malloc(sizeof(hash_table_t));
-      if (!hash_n)
-	return (NULL);
-      hash_n->size = size;
-      hash_n->array;
-      return (NULL);
-    }
-
+	if (!size)
+	{
+		hash_n = malloc(sizeof(hash_table_t));
+		if (!hash_n)
+			return (NULL);
+		hash_n->size = size;
+		hash_n->array;
+		return (NULL);
+	}
 }
