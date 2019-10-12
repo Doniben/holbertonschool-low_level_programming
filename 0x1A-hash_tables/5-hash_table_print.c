5b6828098e86b664bbca3e45e1365ff95024987e
Doniben <doniben@esperanto.co>
2019-10-12 04:43:16 +0000

    [FEATURE] function that prints a hash table + comments and betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table you want to add or update the key
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int index;
