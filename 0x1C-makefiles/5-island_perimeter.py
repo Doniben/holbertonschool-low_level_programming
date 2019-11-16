b2cc02c602054a406170a0f05625028e43f7d380
Doniben <doniben@esperanto.co>
2019-11-16 04:34:56 +0000

    [FIX] indentation - returns the perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,33 +2,33 @@
  """ Island perimeter """
 
 
- def island_perimeter(grid):
-     """ Island perimenter """
+def island_perimeter(grid):
+    """ Island perimenter """
 
-     count = 0
+    count = 0
 
-     for x in range(len(grid)):
-         for y in range(len(grid[x]) - 1):
+    for x in range(len(grid)):
+        for y in range(len(grid[x]) - 1):
 
-             if grid[x][y]:
-                 if (x - 1) < 0:
-                     count += 1
-                 else:
-                     if grid[x - 1][y] == 0:
-                         count += 1
-                 if (y - 1) < 0:
-                     count += 1
-                 else:
-                     if grid[x][y - 1] == 0:
-                         count += 1
-                 if (x + 1) >= len(grid):
-                     count += 1
-                 else:
-                     if grid[x + 1][y] == 0:
-                         count += 1
-                 if (y + 1) >= len(grid):
-                     count += 1
-                 else:
-                     if grid[x][y + 1] == 0:
-                         count += 1
-     return (count)
+            if grid[x][y]:
+                if (x - 1) < 0:
+                    count += 1
+                else:
+                    if grid[x - 1][y] == 0:
+                        count += 1
+                    if (y - 1) < 0:
+                        count += 1
+                else:
+                    if grid[x][y - 1] == 0:
+                        count += 1
+                if (x + 1) >= len(grid):
+                    count += 1
+                else:
+                    if grid[x + 1][y] == 0:
+                        count += 1
+                    if (y + 1) >= len(grid):
+                        count += 1
+                else:
+                    if grid[x][y + 1] == 0:
+                        count += 1
+    return (count)
