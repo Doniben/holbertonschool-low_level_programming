f8ab7c1df5c1c20e1abfe068ae55f873393be3d2
Doniben <doniben@esperanto.co>
2019-12-06 17:53:12 +0000

    [FEATURE] function that measures the height of a binary tree

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -8,13 +8,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counter = 0;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (!tree)
 		return (0);
-	if (tree->right)
-		counter++;
-	if (tree->left)
-		counter++;
-	return (counter);
+	else
+	{
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+	}
+	if (right > left)
+		return (right);
+	else
+		return (left);
+
+
 }
